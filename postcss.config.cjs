7030861e07bd1e75388191eb67372f27e2d4f88d
Yusup Rozimemet <yoseforaz@gmail.com>
2025-10-27 10:44:49 +0100

    updates

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,8 +1,8 @@
+// postcss.config.cjs
 module.exports = {
   plugins: {
-    // Tailwind's PostCSS plugin has been moved to a separate package.
-    // Install `@tailwindcss/postcss` and PostCSS will load it via this key.
-    "@tailwindcss/postcss": {},
+    // Use the new PostCSS plugin package
+    '@tailwindcss/postcss': {},
     autoprefixer: {},
   },
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 import App from "./App.tsx";
+import "./index.css";
 
 // NOTE: All styling (Tailwind / CSS) has been removed per request.
 
